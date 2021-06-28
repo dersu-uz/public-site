290ae7a0c047f55e6d9b2931ad8b9cfc8478e556
kalanda <kalanda@gmail.com>
2021-06-28 20:43:03 +0200

    Linked logo

--- src/components/DersuLogo/index.js
+++ src/components/DersuLogo/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+import Link from 'next/link'
 import DersuQuotes from '../../styles/assets/dersu-quotes.svg'
 
 const DersuLogo = () => {
   return (
     <h1 className="DersuLogo">
-      <DersuQuotes className="DersuLogo__quotes" />
-      <span className="DersuLogo__text">Dersu</span>
+      <Link href="/">
+        <a className="DersuLogo__link">
+          <DersuQuotes className="DersuLogo__quotes" />
+          <span className="DersuLogo__text">Dersu</span>
+        </a>
+      </Link>
     </h1>
   )
 }
