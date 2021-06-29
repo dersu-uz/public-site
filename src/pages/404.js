c573ff67359c2e08a242bc3d39bdbe26ce69f28e
kalanda <kalanda@gmail.com>
2021-06-29 15:50:10 +0200

    404 multilanguage

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ export const getStaticProps = () => {
     props: {
       locale: 'es',
       title: '404',
-      description: 'Página no encontrada',
+      description: 'Página no encontrada / Page not found',
     },
   }
 }
@@ -18,10 +18,17 @@ function Page404() {
   return (
     <DefaultLayout>
       <MarkdownContent>
-        <h2>Página no encontrada</h2>
+        <h2>Página no encontrada / Page not found</h2>
         <p>
           La página que intentas alcanzar, ha caído en una grieta del glaciar.
-          Prueba con la <Link href="/">página principal</Link>
+        </p>
+        <p>
+          The page you are trying to reach has fallen into a crevasse in the
+          glacier.
+        </p>
+        <p>
+          <Link href="/es">Versión en Español</Link> -{' '}
+          <Link href="/en">English version</Link>
         </p>
       </MarkdownContent>
     </DefaultLayout>
