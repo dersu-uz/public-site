900e35cffc6d61dc63ecfffc22b1716764a55feb
Juan Delgado <juan@ustwo.com>
2021-07-12 23:12:18 +0200

    Fixes 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -19,13 +19,8 @@ function Page404() {
     <DefaultLayout>
       <MarkdownContent>
         <h2>Página no encontrada / Page not found</h2>
-        <p>
-          La página que intentas alcanzar, ha caído en una grieta del glaciar.
-        </p>
-        <p>
-          The page you are trying to reach has fallen into a crevasse in the
-          glacier.
-        </p>
+        <p>No encontramos la página que buscas.</p>
+        <p>We cannot find the page you are looking for.</p>
         <p>
           <Link href="/es">Versión en Español</Link> -{' '}
           <Link href="/en">English version</Link>
