bb58f4f59b9094b9a45b7683b7b15254b30e8f38
kalanda <kalanda@gmail.com>
2021-09-21 20:55:05 +0200

    Sticky header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
-import { useContext } from 'react'
+import { useContext, useMemo, useRef } from 'react'
 import Link from 'next/link'
+import { useWindowScroll } from 'react-use'
 
 import TranslationsContext from '@/contexts/TranslationsContext'
 import usePreferredLocale from '@/hooks/usePreferredLocale'
@@ -9,50 +10,84 @@ import Wrapper from '@/components/Wrapper'
 import DersuLogoWithText from '../../styles/assets/dersu-logo-with-text.svg'
 
 const Header = () => {
+  const headerRef = useRef(null)
+  const headerContentRef = useRef(null)
+
   const { localeNames, currentLocale } = useContext(TranslationsContext)
+  const { y: scrollTop } = useWindowScroll()
+
   const { changeLocale } = usePreferredLocale()
+
+  const { isSticky, compensation } = useMemo(() => {
+    const offsetTop =
+      (headerRef && headerRef.current && headerRef.current.offsetTop) || 0
+
+    const distance = scrollTop - offsetTop
+    const isSticky = distance > 0
+    const compensation =
+      (isSticky &&
+        headerContentRef &&
+        headerContentRef.current &&
+        headerContentRef.current.getBoundingClientRect().height) ||
+      0
+
+    return {
+      isSticky,
+      compensation,
+    }
+  }, [scrollTop])
+
   return (
-    <header className="Header">
-      <Container>
-        <Wrapper>
-          <div className="flex py-10 items-center">
-            <h1 className="flex-grow">
-              <Link href={`/${currentLocale}`}>
-                <a>
-                  <DersuLogoWithText className="h-5 -mt-1.5 md:h-6" />
-                </a>
-              </Link>
-            </h1>
-            <nav>
-              <ul className="flex">
-                {localeNames.map((l, i) => {
-                  const isCurrent = l.locale === currentLocale
-                  return (
-                    <li
-                      key={i}
-                      className="font-sans text-dersu-light-gray text-sm pl-2.5 font-bold leading-5 hover:text-dersu-black transition-colors"
-                    >
-                      {isCurrent ? (
-                        <span className="text-dersu-black">{l.name}</span>
-                      ) : (
-                        <a
-                          href={`/${l.locale}/`}
-                          onClick={e => {
-                            e.preventDefault()
-                            changeLocale(l.locale)
-                          }}
-                        >
-                          {l.name}
-                        </a>
-                      )}
-                    </li>
-                  )
-                })}
-              </ul>
-            </nav>
-          </div>
-        </Wrapper>
-      </Container>
+    <header
+      className="Header"
+      ref={headerRef}
+      style={{ paddingTop: `${compensation}px` }}
+    >
+      <div
+        ref={headerContentRef}
+        className={`${isSticky ? 'fixed top-0 left-0 w-full z-10' : ''}`}
+      >
+        <Container>
+          <Wrapper>
+            <div className="flex py-10 items-center">
+              <h1 className="flex-grow">
+                <Link href={`/${currentLocale}`}>
+                  <a>
+                    <DersuLogoWithText className="h-5 -mt-1.5 md:h-6" />
+                  </a>
+                </Link>
+              </h1>
+              <nav>
+                <ul className="flex">
+                  {localeNames.map((l, i) => {
+                    const isCurrent = l.locale === currentLocale
+                    return (
+                      <li
+                        key={i}
+                        className="font-sans text-dersu-light-gray text-sm pl-2.5 font-bold leading-5 hover:text-dersu-black transition-colors"
+                      >
+                        {isCurrent ? (
+                          <span className="text-dersu-black">{l.name}</span>
+                        ) : (
+                          <a
+                            href={`/${l.locale}/`}
+                            onClick={e => {
+                              e.preventDefault()
+                              changeLocale(l.locale)
+                            }}
+                          >
+                            {l.name}
+                          </a>
+                        )}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </nav>
+            </div>
+          </Wrapper>
+        </Container>
+      </div>
     </header>
   )
 }
