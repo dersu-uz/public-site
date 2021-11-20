4fc81ecb239cff1cadf8f843b19f7def6ae833b2
kalanda <kalanda@gmail.com>
2021-11-20 09:57:25 +0100

    take base url from Vercel when is in a branch preview deployment

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,6 +1,7 @@
 /* global process*/
 
-export const BASE_DOMAIN_URL = process.env.NEXT_PUBLIC_BASE_DOMAIN_URL
+export const BASE_DOMAIN_URL =
+  process.env.VERCEL_URL || process.env.NEXT_PUBLIC_BASE_DOMAIN_URL
 export const IUBENDA_ID = `82963742`
 export const IUBENDA_PRIVACY_API_URL = `https://www.iubenda.com/api/privacy-policy/${IUBENDA_ID}/no-markup`
 export const IUBENDA_PRIVACY_LINK = `<a href="https://www.iubenda.com/privacy-policy/${IUBENDA_ID}" class="iubenda-white no-brand iubenda-noiframe iubenda-embed iubenda-noiframe iub-body-embed" title="Privacy Policy">Privacy Policy</a>`
