60473d8a072d754b3e76186dd9ee96f738760e9d
Kalanda <kalanda@gmail.com>
2021-12-23 10:08:54 +0100

    Blog Análisis de una salida (#43)
    Co-authored-by: Juan Delgado <jd@juandelgado.es>

--- src/components/MarkdownContent/index.tsx
+++ src/components/MarkdownContent/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 const MarkdownContent: FC<Props> = ({ dangerouslySetInnerHTML, children }) => {
   return (
     <div
-      className="prose prose-blockquote:border-dersu-pink sm:prose-xl"
+      className="prose prose-blockquote:border-dersu-pink sm:prose-xl prose-img:w-full"
       dangerouslySetInnerHTML={dangerouslySetInnerHTML}
     >
       {children}
