cd10b1245f832f8a88c73034ab78e27671d6e317
kalanda <kalanda@gmail.com>
2022-05-12 20:59:28 +0200

    Added french version

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,9 +1,11 @@
 import en from './en.json'
 import es from './es.json'
+import fr from './fr.json'
 
 const locales = {
   es,
   en,
+  fr,
 }
 
 export default locales
---
+++ src/pages/fr/index.tsx
@@ -0,0 +1,50 @@
+import { FC } from 'react'
+import { GetStaticProps } from 'next'
+
+import { getLatestPosts } from '@/services/blogService'
+
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+
+import HomeIntro from '@/modules/HomeIntro'
+import HomeHero from '@/modules/HomeHero'
+import Signup from '@/modules/Signup'
+import HomeMountaineerProfile from '@/modules/HomeMountaineerProfile'
+import HomeRoutePlanning from '@/modules/HomeRoutePlanning'
+import HomeRouteAlerts from '@/modules/HomeRouteAlerts'
+import HomeLearn from '@/modules/HomeLearn'
+import RelatedPosts from '@/modules/RelatedPosts'
+import { ENABLE_APP_DOWNLOAD } from '@/constants/settings'
+
+export const getStaticProps: GetStaticProps = async () => {
+  const latestPosts = await getLatestPosts('fr', 2)
+  return {
+    props: {
+      locale: 'fr',
+      description: 'Dersu connecte les gens avec les montaignes',
+      latestPosts,
+    },
+  }
+}
+
+type Props = {
+  latestPosts: Array<BlogPost>
+}
+
+const HomePage: FC<Props> = ({ latestPosts }) => (
+  <>
+    <HomeHero />
+    <Header />
+    <HomeIntro />
+    <HomeMountaineerProfile />
+    <HomeRoutePlanning />
+    {ENABLE_APP_DOWNLOAD && <Signup />}
+    <HomeRouteAlerts />
+    <HomeLearn />
+    {!ENABLE_APP_DOWNLOAD && <Signup />}
+    <RelatedPosts posts={latestPosts} />
+    <Footer />
+  </>
+)
+
+export default HomePage
