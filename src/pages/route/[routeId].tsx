e4871b5cc8f1337dcb0c1b53ae6ba6b70c526e69
Kossy Ugochukwu <kossyugochukwu@gmail.com>
2022-06-14 12:28:03 +0100

    add page titles

--- src/pages/route/[routeId].tsx
+++ src/pages/route/[routeId].tsx
@@ -2,7 +2,16 @@ import Header from '@/components/Header'
 import Picture from '@/components/Picture'
 import StoreButtons from '@/components/StoreButtones'
 import TranslationsContext from '@/contexts/TranslationsContext'
+import { GetServerSideProps } from 'next'
 import { useContext } from 'react'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+    props: {
+      title: 'Route',
+    },
+  }
+}
 
 const Route = () => {
   const { t } = useContext(TranslationsContext)
@@ -10,7 +19,7 @@ const Route = () => {
   return (
     <div className="w-[100%] h-[100vh] bg-dersu-blue">
       <Header />
-      <div className="flex flex-col md:flex-row pt-16 px-8 justify-center md:justify-between m-auto w-100% items-center mx-w-xl">
+      <div className="flex flex-col md:flex-row pt-16 px-8 justify-center md:justify-between m-auto  items-center max-w-screen-2xl">
         <div className="text-dersu-brown">
           <p className="text-dersu-xl text-center md:text-left mb-8 mx-auto text-dersu-lg lg:text-dersu-3xl md:text-dersu-2xl md:max-w-xl">
             {t.routeFallback.message}
