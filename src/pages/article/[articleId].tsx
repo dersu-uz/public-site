258141869ba5595bc95ca2140f3657e9a369e19f
Maria Situmbeko <mariasitumbeko2@gmail.com>
2022-07-21 11:38:11 +0200

    Created Views folder

--- src/pages/article/[articleId].tsx
+++ src/pages/article/[articleId].tsx
@@ -1,10 +1,6 @@
-import Header from '@/components/Header'
-import Picture from '@/components/Picture'
-import StoreButtons from '@/components/StoreButtons'
-import TranslationsContext from '@/contexts/TranslationsContext'
 import { getTranslations, LocaleShortCode } from '@/services/i18nService'
+import ArticleFallback from '@/views/Article'
 import { GetServerSideProps } from 'next'
-import { useContext } from 'react'
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const t = getTranslations(ctx.locale as LocaleShortCode)
@@ -16,33 +12,8 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
   }
 }
 
-const ArticleFallback = () => {
-  const { t } = useContext(TranslationsContext)
-
-  return (
-    <div className="w-[100%] h-[100vh] bg-dersu-blue">
-      <Header />
-      <div className="flex flex-col md:flex-row pt-16 px-8 justify-center md:justify-between m-auto  items-center max-w-screen-2xl">
-        <div className="text-dersu-brown">
-          <p className="text-dersu-xl text-center md:text-left mb-8 mx-auto lg:text-dersu-3xl md:text-dersu-2xl md:max-w-xl">
-            {t.articleFallback.message}
-          </p>
-          <StoreButtons />
-        </div>
-
-        <div>
-          <Picture
-            alt="Dersu App"
-            path="/images/views/route-fallback"
-            imageName="picture-1"
-            imageType="png"
-            densities={[1, 2]}
-            imgClassName=""
-          />
-        </div>
-      </div>
-    </div>
-  )
+const ArticleFallbackPage = () => {
+  return <ArticleFallback />
 }
 
-export default ArticleFallback
+export default ArticleFallbackPage
--- src/pages/learning/article/[articleId].tsx
+++ src/pages/learning/article/[articleId].tsx
@@ -1,10 +1,6 @@
-import Header from '@/components/Header'
-import Picture from '@/components/Picture'
-import StoreButtons from '@/components/StoreButtones'
-import TranslationsContext from '@/contexts/TranslationsContext'
 import { getTranslations, LocaleShortCode } from '@/services/i18nService'
+import Article from '@/views/Learning'
 import { GetServerSideProps } from 'next'
-import { useContext } from 'react'
 
 export const getServerSideProps: GetServerSideProps = async ctx => {
   const t = getTranslations(ctx.locale as LocaleShortCode)
@@ -18,33 +14,8 @@ export const getServerSideProps: GetServerSideProps = async ctx => {
   }
 }
 
-const Article = () => {
-  const { t } = useContext(TranslationsContext)
-
-  return (
-    <div className="w-[100%] h-[100vh] bg-dersu-blue">
-      <Header />
-      <div className="flex flex-col md:flex-row pt-16 px-8 justify-center md:justify-between m-auto  items-center max-w-screen-2xl">
-        <div className="text-dersu-brown">
-          <p className="text-dersu-xl text-center md:text-left mb-8 mx-auto text-dersu-lg lg:text-dersu-3xl md:text-dersu-2xl md:max-w-xl">
-            {t.articleFallback.message}
-          </p>
-          <StoreButtons />
-        </div>
-
-        <div>
-          <Picture
-            alt="Dersu App"
-            path="/images/views/route-fallback"
-            imageName="picture-1"
-            imageType="png"
-            densities={[1, 2]}
-            imgClassName=""
-          />
-        </div>
-      </div>
-    </div>
-  )
+const ArticlePage = () => {
+  return <Article />
 }
 
-export default Article
+export default ArticlePage
